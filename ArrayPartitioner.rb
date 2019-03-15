d0d2e1e2b33c1b068f686ca889740740ce75b1a0
lopezp9492 <43254797+lopezp9492@users.noreply.github.com>
2019-03-14 22:22:04 -0700

    Removed Debug Comments

--- ArrayPartitioner.rb
+++ ArrayPartitioner.rb
@@ -7,41 +7,15 @@
 # reorders an array based on the given test value
 def partition(array, testValue)
 
-	# Debug
-	tag = "partition."
-	puts "\n"
-	puts tag + "(array, testValue)"
-	puts tag + "(#{array}, #{testValue})"
 
 	left_index = 0
 	right_index = array.length-1
 
-	#Debug
-	puts "left_index: #{left_index}"
-	puts "right_index: #{right_index}"
-
-	#Debug 
-	puts "\n"
-	l_value = array[left_index]
-	r_value = array[right_index]
-	puts "array[left_index] #{l_value}"
-	puts "array[right_index] #{r_value}"
-
 	# While markers have not crossed each other
 	while(left_index < right_index)
 
-		#Debug 
-		puts "\n---------------"
 		l_value = array[left_index]
 		r_value = array[right_index]
-		puts "array[left_index] #{l_value}"
-		puts "array[right_index] #{r_value}"
-
-		# Debug
-		puts "left_index: #{left_index}"
-		puts "right_index: #{right_index}" 
-		#sleep(1) # wait x seconds before closing program
-
 		if(l_value > testValue and r_value < testValue)	
 			# Swap
 			temp = array[left_index]
@@ -51,28 +25,21 @@ def partition(array, testValue)
 
 		#Move left_index forward
 		while(array[left_index]<testValue)
-			puts"move forward"
 			left_index = left_index+1
 		end
 
 		#Move right_index back
 		while(array[right_index]>testValue)
-			puts"move back"
 			right_index = right_index-1
 		end
 	end
-
-	# Debug
-	puts array.inspect
 
 	#returns the point where the test value divides the array
 	if(array[left_index]>testValue)
 		left_index -= 1
 	end
 	return left_index, array
-
 end
-
 
 # Variables
 array = []
@@ -106,5 +73,5 @@ p_index, array = partition(array, testValue)
 puts "\n---------------"
 puts "Result"
 puts array.inspect
-puts "p index #{p_index}"
+puts "p index = #{p_index}"
 sleep(10) # wait x seconds before closing program
